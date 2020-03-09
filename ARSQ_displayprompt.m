33c818bb617112032bc89b062f6dc6de478be9a1
geffent <57996702+geffent@users.noreply.github.com>
2020-03-09 23:26:23 +0100

    Adapt display prompts to local needs

--- ARSQ_displayprompt.m
+++ ARSQ_displayprompt.m
@@ -1,11 +1,16 @@
-function [response] = ARSQ_displayprompt(ARSQ_item,window,screenXpixels,screenYpixels,handedness)
+
+function [response] = ARSQ_displayprompt(ARSQ_item, window, screenXpixels, screenYpixels, handedness)
 Screen('TextFont', window, 'Times');
 if strcmp(handedness, 'left') % 5-1 for left handed
-    x_locs = [screenXpixels*0.90, screenXpixels*0.70, screenXpixels*0.50, screenXpixels*0.30,screenXpixels*0.10];
+    x_locs = [screenXpixels*0.80, screenXpixels*0.60, screenXpixels*0.40, screenXpixels*0.20];
 elseif strcmp (handedness, 'right') % 1-5 for right handed
-    x_locs = [screenXpixels*0.10, screenXpixels*0.30, screenXpixels*0.50, screenXpixels*0.70,screenXpixels*0.90];
+    x_locs = [screenXpixels*0.20, screenXpixels*0.40, screenXpixels*0.60, screenXpixels*0.80];
 end
+
+% i is the number of the current question
+response = [];  % make sure we have a variable to store all answers in
 for i = 1:length(ARSQ_item)
+
     a = ARSQ_item(i);
     Screen('TextSize', window, 80);
     DrawFormattedText(window, WrapString(a{1},30),'center',screenYpixels*0.5, [1 1 1]);
@@ -13,12 +18,11 @@ for i = 1:length(ARSQ_item)
     DrawFormattedText(window,'2',x_locs(2),screenYpixels*0.75, [1 1 1]);
     DrawFormattedText(window,'3',x_locs(3),screenYpixels*0.75, [1 1 1]);
     DrawFormattedText(window,'4',x_locs(4),screenYpixels*0.75, [1 1 1]);
-    DrawFormattedText(window,'5',x_locs(5),screenYpixels*0.75, [1 1 1]);
     Screen('TextSize', window, 50);
     DrawFormattedText(window,'Strongly\nDisagree',x_locs(1)-screenXpixels*.05,screenYpixels*0.85, [1 1 1]);
-    DrawFormattedText(window,'Neutral','center',screenYpixels*0.85, [1 1 1]);
-    DrawFormattedText(window,'Strongly\nAgree',x_locs(5)-screenXpixels*.05,screenYpixels*0.85, [1 1 1]);
-    
+    %DrawFormattedText(window,'Neutral','center',screenYpixels*0.85, [1 1 1]);
+    DrawFormattedText(window,'Strongly\nAgree',x_locs(4)-screenXpixels*.05,screenYpixels*0.85, [1 1 1]);
+
     Screen('Flip', window);
     
     while KbCheck; end % Wait until all keys are released.
@@ -30,24 +34,41 @@ for i = 1:length(ARSQ_item)
             v = find(keyCode);
             keyname = KbName(v);
             if strcmp(handedness, 'right') % checks keys for right-handed participants
-                if strcmp(keyname, '1!') || strcmp(keyname, '2@') || strcmp(keyname, '3#') || strcmp(keyname, '4$') || strcmp(keyname, '5%')
+                
+                if strcmp(keyname, '1!') || strcmp(keyname, '2@') ...
+                        || strcmp(keyname, '3#') || strcmp(keyname, '4$')
+                    
                     response(i,:) = str2num(keyname(1)); %#ok
-                    break;
-                end
-            elseif strcmp(handedness ,'left') % checks keys for left-handed participants
-                if strcmp(keyname, '6^') || strcmp(keyname, '7&') || strcmp(keyname, '8*') || strcmp(keyname, '9(') || strcmp(keyname, 'a')
-                    if strcmp(keyname, '6^')
-                        response(i,:) = 1; %#ok
-                    elseif strcmp(keyname, '7&')
-                        response(i,:) = 2; %#ok
-                    elseif strcmp(keyname, '8*')
-                        response(i,:) = 3; %#ok
-                    elseif strcmp(keyname, '9(')
-                        response(i,:) = 4; %#ok
-                    elseif strcmp(keyname, 'a')
-                        response(i,:) = 5; %#ok
+                         
+                    % redraw the whole screen marking the chosen answer
+                    Screen('TextSize', window, 80);
+                    DrawFormattedText(window, WrapString(a{1},30),'center',screenYpixels*0.5, [1 1 1]);
+                    % paint the chosen number in green, the others in black
+                    for j = 1:4
+                        if str2num(keyname(1)) == j
+                            DrawFormattedText(window,num2str(j),x_locs(j),screenYpixels*0.75, [0 1 0]);
+                        else
+                            DrawFormattedText(window,num2str(j),x_locs(j),screenYpixels*0.75, [1 1 1]);
+                        end
                     end
-                    break;
+                    Screen('TextSize', window, 50);
+                    DrawFormattedText(window,'Strongly\nDisagree',x_locs(1)-screenXpixels*.05,screenYpixels*0.85, [1 1 1]);
+                    %DrawFormattedText(window,'Neutral','center',screenYpixels*0.85, [1 1 1]);
+                    DrawFormattedText(window,'Strongly\nAgree',x_locs(4)-screenXpixels*.05,screenYpixels*0.85, [1 1 1]);
+
+                    Screen('Flip', window);      
+                end
+                
+                % Check if Return was pressed _and_ subject gave valid
+                % answer
+                numberOfQuestionsAsked = i;
+                numberOfValidAnswersGiven = size(response, 1);
+                if strcmp(keyname, 'Return') && ...
+                    numberOfValidAnswersGiven == numberOfQuestionsAsked
+                    res = response(i, :);
+                    if (res == 1 || res == 2 || res == 3 || res == 4)
+                        break;
+                    end
                 end
             end
         end
@@ -57,4 +78,4 @@ end
 % put up ending instructions
 DrawFormattedText(window, WrapString('Please continue to stay still for the remainder of the scan. \n\nThank you.',50),'center','center', [1 1 1]);
 Screen('Flip', window);
-WaitSecs(10); % Delay before ending
+WaitSecs(2); % Delay before ending
