fca7c8ca85f5fc942db93c4f3fb6fc4094e29bbc
geffent <57996702+geffent@users.noreply.github.com>
2020-03-15 23:26:23 +0100

    Update README.m

--- README.m
+++ README.m
@@ -1,6 +1,6 @@
 % Heavily adapted from https://github.com/hek26/ARSQ
 
-% To launch the task, run ARSQ('subjectID')
+% To launch the task, just run `ARSQ()`
 
 % The output is an excel spreadshit with the subject's answers.
 % One file per subject.
